1492276dc56200c3019bc307bc837c0a883f4491
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-04-02 21:31:03 +0900

    S3の設定

---
+++ common/s3.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "iam_ssh" {
+    bucket = "${var.project}-iam-ssh"
+
+    tags = {
+        Name = var.project
+        Group = var.project
+    }
+}
+
+// ファイルをs3にアップする
+resource "aws_s3_bucket_object" "iam_ssh_install_script" {
+    bucket = aws_s3_bucket.iam_ssh.id
+    key = "install_iam_ssh.sh"
+    source = "files/iam_ssh/install_iam_ssh.sh"
+    etag = filemd5("files/iam_ssh/install_iam_ssh.sh")
+}
+
+// ファイルをs3にアップする
+resource "aws_s3_bucket_object" "iam_ssh_config" {
+    bucket = aws_s3_bucket.iam_ssh.id
+    key = "aws-ec2-ssh.conf"
+    source = "files/iam_ssh/aws-ec2-ssh.conf"
+    etag = filemd5("files/iam_ssh/aws-ec2-ssh.conf")
+}
