13518c838186a6f7f180d99825f3ef9ff70386a1
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-04-04 00:15:01 +0900

    軽微な修正

--- prod/cloudfront.tf
+++ prod/cloudfront.tf
@@ -1,7 +1,7 @@
 # Cloud Frontとは、キャッシュをすることでウェブコンテンツの配信速度を高速化するサービス
 resource "aws_cloudfront_distribution" "web" {
   # 有効かどうか
-  enabled         = true
+  enabled = true
   # IPv6通信を有効にするか
   is_ipv6_enabled = true
   comment         = "For ${var.project} Web files(${var.env})"
@@ -28,7 +28,7 @@ resource "aws_cloudfront_distribution" "web" {
     # DNSドメイン名
     domain_name = aws_s3_bucket.assets.bucket_regional_domain_name
     # オリジンを識別するユニークなID
-    origin_id   = aws_s3_bucket.assets.bucket
+    origin_id = aws_s3_bucket.assets.bucket
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.web_assets.cloudfront_access_identity_path
@@ -84,7 +84,7 @@ resource "aws_cloudfront_distribution" "web" {
     # 転送するリクエストデータ
     forwarded_values {
       # キャッシュ動作に関連するオリジンにクエリ文字列を転送するか
-      query_string            = false
+      query_string = false
       # 上のquery_string に true を指定すると、すべてのクエリ文字列が転送されるが、転送するクエリ文字列を制限したい場合、この引数にリストされたクエリ文字列キーのみがキャッシュされる
       query_string_cache_keys = []
       # CloudFrontがこのキャッシュ動作のために変化させたいHeaderを指定する。すべてのヘッダーを含める場合は * を指定する。
@@ -152,18 +152,18 @@ resource "aws_cloudfront_distribution" "web" {
       "HEAD",
     ]
     # リクエストヘッダにAccept-Encoding: gzipを含むWebリクエストに対して、CloudFrontが自動的にコンテンツを圧縮するかどうか
-    compress               = false
+    compress = false
     #  Cache-Control max-ageまたはExpiresヘッダーがない場合、CloudFrontが別のリクエストを転送する前にオブジェクトがCloudFrontキャッシュにあるデフォルトの時間(秒)
-    default_ttl            = 86400
+    default_ttl = 86400
     # CloudFrontがあなたのオリジンに別のリクエストを転送し、オブジェクトが更新されたかどうかを判断するまでの、CloudFrontキャッシュ内にある最大時間（単位：秒）(Cache-Control max-age、Cache-Control s-maxage、Expiresヘッダがある場合のみ有効)
-    max_ttl                = 259200
+    max_ttl = 259200
     # CloudFrontがオリジンにオブジェクトが更新されたかどうかを問い合わせる前に、オブジェクトがCloudFrontキャッシュに留まるようにする最小時間(単位：秒)
-    min_ttl                = 0
+    min_ttl = 0
     # このキャッシュ動作に関連付けられたオリジンを使用して、Microsoft Smooth Streaming 形式でメディア ファイルを配布するかどうか
-    smooth_streaming       = false
-    target_origin_id       = aws_lb.web.name
+    smooth_streaming = false
+    target_origin_id = aws_lb.web.name
     # プライベートコンテンツの署名付きURLの作成を許可するAWSアカウントIDのリスト
-    trusted_signers        = []
+    trusted_signers = []
     # この要素を使用して、要求が PathPattern のパスパターンに一致する場合に、TargetOriginId で指定したオリジンのファイルにアクセスするためにユーザーが使用できるプロトコルを指定する。allow-all、https-only、または redirect-to-https のいずれかを指定する。
     viewer_protocol_policy = "redirect-to-https"
 
@@ -202,7 +202,7 @@ resource "aws_cloudfront_origin_access_identity" "web_assets" {
 
 resource "aws_cloudfront_distribution" "admin" {
   # 有効かどうか
-  enabled         = true
+  enabled = true
   # IPv6通信を有効にするか
   is_ipv6_enabled = true
   comment         = "For ${var.project} Admin files(${var.env})"
@@ -234,7 +234,7 @@ resource "aws_cloudfront_distribution" "admin" {
     # DNSドメイン名
     domain_name = aws_s3_bucket.uploads.bucket_regional_domain_name
     # オリジンを識別するユニークな名前
-    origin_id   = aws_s3_bucket.uploads.bucket
+    origin_id = aws_s3_bucket.uploads.bucket
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.web_uploads.cloudfront_access_identity_path
